d5bf4eeecaf400381937dcd295d04f5a9e940160
Markus Ecker <markus.ecker@gmail.com>
2024-03-22 13:38:39 +0100

    add basic dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={inter.className + " h-full"}>{children}</body>
     </html>
   );
 }
