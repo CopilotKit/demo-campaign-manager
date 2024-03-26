a4981e57d7567f343db9350fc977a651852f2665
Markus Ecker <markus.ecker@gmail.com>
2024-03-26 15:07:55 +0100

    update env variables

--- src/app/api/copilotkit/route.ts
+++ src/app/api/copilotkit/route.ts
@@ -4,5 +4,7 @@ export const runtime = "edge";
 
 export async function POST(req: Request): Promise<Response> {
   const copilotKit = new CopilotBackend({});
-  return copilotKit.response(req, new OpenAIAdapter());
+  const openaiModel = process.env["OPENAI_MODEL"];
+
+  return copilotKit.response(req, new OpenAIAdapter({ model: openaiModel }));
 }
